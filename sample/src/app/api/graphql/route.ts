0baf51bb1152bcd02ce839642986df5bd66fe987
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-06-13 23:43:43 +0900

    add: todo backend

--- sample/src/app/api/graphql/route.ts
+++ sample/src/app/api/graphql/route.ts
@@ -1,16 +1,41 @@
 import { createYoga, createSchema } from 'graphql-yoga'
 
+const todos = [
+  { id: 1, text: 'Buy milk', completed: false },
+  { id: 2, text: 'Buy eggs', completed: false },
+  { id: 3, text: 'Buy bread', completed: false },
+]
+
 const { handleRequest } = createYoga({
   schema: createSchema({
     typeDefs: /* GraphQL */ `
+      type Todo {
+        id: ID!
+        text: String!
+        completed: Boolean!
+      }
+
       type Query {
         greetings: String
+        todos: [Todo!]!
+      }
+
+      type Mutation {
+        addTodo(text: String!): Todo!
       }
     `,
     resolvers: {
       Query: {
         greetings: () =>
           'This is the `greetings` field of the root `Query` type',
+        todos: () => todos,
+      },
+      Mutation: {
+        addTodo: (_, { text }) => {
+          const todo = { id: todos.length + 1, text, completed: false }
+          todos.push(todo)
+          return todo
+        },
       },
     },
   }),
